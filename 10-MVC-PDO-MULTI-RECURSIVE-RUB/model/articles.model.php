65a71f9c24f239df5c253b4ee0eb43173815b1eb
mikhawa <michaeljpitz@gmail.com>
2020-11-20 13:52:32 +0100

    request for get rubriques with articles

--- 10-MVC-PDO-MULTI-RECURSIVE-RUB/model/articles.model.php
+++ 10-MVC-PDO-MULTI-RECURSIVE-RUB/model/articles.model.php
@@ -2,11 +2,15 @@
 // All articles
 function selectAllArticles(PDO $c){
     // sélection de tous les articles sur l'accueil
-    $sql = "SELECT a.* FROM articles a 
-        INNER JOIN articles_has_rubriques h
-        ON h.articles_idarticles = a.idarticles
-        INNER JOIN rubriques r
-        ON h.rubriques_idrubriques = r.idrubriques
+    $sql = "SELECT a.*, 
+       GROUP_CONCAT(r.idrubriques) AS idrubriques, 
+       GROUP_CONCAT(r.rubriques_name SEPARATOR '|||') AS rubriques_name
+        FROM articles a 
+            INNER JOIN articles_has_rubriques h
+                ON h.articles_idarticles = a.idarticles
+            INNER JOIN rubriques r
+                ON h.rubriques_idrubriques = r.idrubriques
+        GROUP BY a.idarticles
         ORDER BY a.articles_date DESC";
     $request = $c->query($sql);
     return ($request->rowCount()) ? $request->fetchAll(PDO::FETCH_ASSOC) : [];
